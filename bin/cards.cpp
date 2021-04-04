a3bc978e1239d93d8cf51106f1aabb50c831f44d
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-04-04 15:18:21 +0200

    100/100

--- bin/cards.cpp
+++ bin/cards.cpp
@@ -1,29 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define DEBUG
+//#define DEBUG
 
 long long solve(long long N, long long R, long long C)
 {
-    int counter = 0;
-    int mymin, mymax;
-    mymin = mymax = C - 1;
-
-    for (int i = R - 1; i >= 0; --i)
-    {
-        counter += mymax - mymin + 1;
-#ifdef DEBUG
-        cout << i << "\t" << mymin << "\t" << mymax << "\t" << counter << endl;
-#endif // DEBUG
-        if (mymax == i && mymin == 0)
-            return counter + (i) * (i + 1) / 2;
-        if (mymin != 0)
-            mymin--;
-        if (mymax == i)
-            mymax--;
-    }
-
-    return counter;
+    return C*(R-C+1);
 }
 
 int main()
